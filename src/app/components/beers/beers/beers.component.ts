ad25625ecb170fbdf7a5a78e91dd4e43591326a9
Daniel <danielrojo@gmail.com>
2022-07-07 11:53:24 +0200

    beers filtered and ordered

--- src/app/components/beers/beers/beers.component.ts
+++ src/app/components/beers/beers/beers.component.ts
@@ -18,7 +18,12 @@ export class BeersComponent implements OnInit {
     step: 0.1,
   };
 
-  counter = 0;
+  order = 0;
+  ascendent = true;
+
+  orderString = ''
+  ascendentString = ''
+
 
   beers: Beer[] = [];
   filteredBeers: Beer[] = [];
@@ -26,6 +31,8 @@ export class BeersComponent implements OnInit {
   constructor(private service: BeersService) { }
 
   ngOnInit(): void {
+    this.getOrderString();
+    this.getAscendentString();
     this.service.beers$.subscribe(beers => {
       this.beers = beers;
       this.getFilteredBeers();
@@ -38,12 +45,64 @@ export class BeersComponent implements OnInit {
     this.getFilteredBeers();
   }
 
-  getFilteredBeers(): void {
-    console.log('getFilteredBeers: ' + this.counter);
-    this.counter++;
-    this.filteredBeers = this.beers.filter(beer => {
+  private getFilteredBeers(): void {
+    this.filteredBeers = this.beers
+    .filter(beer => {
       return beer.abv >= this.value && beer.abv <= this.highValue;
-    });
+    })
+    .sort((a: Beer, b: Beer) => {
+      if (this.order === 0) {
+        // order by name
+        if (this.ascendent) {
+          return a.name.localeCompare(b.name);
+        } else {
+          return b.name.localeCompare(a.name);
+        }
+      } else if (this.order === 1) {
+        // order by abv
+        if (this.ascendent) {
+          return a.abv - b.abv;
+        } else {
+          return b.abv - a.abv;
+        }
+      }
+      else {
+        // order by ibu
+        if (this.ascendent) {
+          return a.abv - b.abv;
+        } else {
+          return b.abv - a.abv;
+        }
+      }
+  });
+  }
+
+  handleOrder(value: number) {
+    console.log(value);
+    this.order = value;
+    this.getFilteredBeers();
+    this.getOrderString();
+  }
+
+  handleAsc(value: boolean) {
+    console.log(value);
+    this.ascendent = value;
+    this.getFilteredBeers();
+    this.getAscendentString();
+  }
+
+  getOrderString(){
+    if (this.order === 0) {
+      this.orderString = 'Name';
+    } else if (this.order === 1) {
+      this.orderString = 'ABV';
+    } else {
+      this.orderString = 'NONE';
+    }
+  }
+
+  getAscendentString() {
+    this.ascendent ? this.ascendentString = 'Ascendent' : this.ascendentString = 'Descendent';
   }
 
 }
