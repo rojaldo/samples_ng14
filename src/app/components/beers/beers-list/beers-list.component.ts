caba05729eef3bd15c7bdde1325f39f527ccb3c7
Daniel <danielrojo@gmail.com>
2022-07-07 13:00:31 +0200

    beers filtered with several components

--- src/app/components/beers/beers-list/beers-list.component.ts
+++ src/app/components/beers/beers-list/beers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Beer } from 'src/app/models/beer';
 
 @Component({
@@ -6,14 +6,64 @@ import { Beer } from 'src/app/models/beer';
   templateUrl: './beers-list.component.html',
   styleUrls: ['./beers-list.component.scss']
 })
-export class BeersListComponent implements OnInit {
-
+export class BeersListComponent implements OnInit, OnChanges {
 
   @Input() beers: Beer[] = [];
+  @Input() selection: any = {
+    order: 0,
+    ascendent: true,
+    value: 4,
+    highValue: 6
+  }
+
+  filteredBeers: Beer[] = [];
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selection']) {
+      this.getFilteredBeers();
+    } else if (changes['beers']) {
+      this.getFilteredBeers();
+    }
+
+  }
 
   ngOnInit(): void {
+
   }
+
+  getFilteredBeers(): void {
+    
+    this.filteredBeers = this.beers
+      .filter(beer => {
+        return beer.abv >= this.selection.value && beer.abv <= this.selection.highValue;
+      })
+      .sort((a: Beer, b: Beer) => {
+        if (this.selection.order === 0) {
+          // order by name
+          if (this.selection.ascendent) {
+            return a.name.localeCompare(b.name);
+          } else {
+            return b.name.localeCompare(a.name);
+          }
+        } else if (this.selection.order === 1) {
+          // order by abv
+          if (this.selection.ascendent) {
+            return a.abv - b.abv;
+          } else {
+            return b.abv - a.abv;
+          }
+        }
+        else {
+          // order by ibu
+          if (this.selection.ascendent) {
+            return a.abv - b.abv;
+          } else {
+            return b.abv - a.abv;
+          }
+        }
+      });
+  }
+
 
 }
--- src/app/components/beers/beers/beers.component.ts
+++ src/app/components/beers/beers/beers.component.ts
@@ -18,53 +18,19 @@ export class BeersComponent implements OnInit {
   }
 
   beers: Beer[] = [];
-  filteredBeers: Beer[] = [];
 
   constructor(private service: BeersService) { }
 
   ngOnInit(): void {
     this.service.beers$.subscribe(beers => {
       this.beers = beers;
-      this.getFilteredBeers();
     });
     this.service.getBeers();
   }
 
-  private getFilteredBeers(): void {
-    this.filteredBeers = this.beers
-    .filter(beer => {
-      return beer.abv >= this.selection.value && beer.abv <= this.selection.highValue;
-    })
-    .sort((a: Beer, b: Beer) => {
-      if (this.selection.order === 0) {
-        // order by name
-        if (this.selection.ascendent) {
-          return a.name.localeCompare(b.name);
-        } else {
-          return b.name.localeCompare(a.name);
-        }
-      } else if (this.selection.order === 1) {
-        // order by abv
-        if (this.selection.ascendent) {
-          return a.abv - b.abv;
-        } else {
-          return b.abv - a.abv;
-        }
-      }
-      else {
-        // order by ibu
-        if (this.selection.ascendent) {
-          return a.abv - b.abv;
-        } else {
-          return b.abv - a.abv;
-        }
-      }
-  });
-  }
 
   handleSelectionChange(event: any){
     this.selection = event;
-    this.getFilteredBeers();
   }
 
 }
